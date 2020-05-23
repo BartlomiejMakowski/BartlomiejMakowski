c84ac98ef37d759c69342a2e76a2ee83c0c9eaf1
BartlomiejMakowski <62451207+BartlomiejMakowski@users.noreply.github.com>
2020-05-23 20:48:52 +0200

    Update Kalkulator.cpp

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -1,4 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <iostream>
 #include <stdio.h>
 
 void dodawanie()
@@ -6,10 +16,10 @@ void dodawanie()
     int a;
     int b;
     int wynik;
-     printf_s("Podaj liczbę pierwsza oraz druga\n");
-     scanf_s("%d, %d" & a, &b);
-         a + b = wynik;
-         printf_s("Wynik dodawnia wynosi:%d\n",wynik);
+    printf_s("Podaj liczbę pierwsza oraz druga(Po przecinku)\n");
+    scanf_s("%d, %d", &a, &b);
+    wynik = a + b;
+    printf_s("Wynik dodawnia wynosi:%d\n", wynik);
 
 }
 
@@ -18,9 +28,9 @@ void odejmowanie()
     int a;
     int b;
     int wynik;
-    printf_s("Podaj liczbę pierwsza oraz druga\n");
-    scanf_s("%d, %d" & a, &b);
-    a - b = wynik;
+    printf_s("Podaj liczbę pierwsza oraz druga(Po przecinku)\n");
+    scanf_s("%d, %d", &a, &b);
+     wynik = a - b ;
     printf_s("Wynik odejmowania wynosi:%d\n", wynik);
 
 }
@@ -29,27 +39,27 @@ void mnozenie()
     int a;
     int b;
     int wynik;
-    printf_s("Podaj liczbę pierwsza oraz druga\n");
-    scanf_s("%d, %d" & a, &b);
-    a * b = wynik;
+   printf_s("Podaj liczbę pierwsza oraz druga(Po przecinku)\n");
+    scanf_s("%d, %d", &a, &b);
+     wynik = a * b;
     printf_s("Wynik mnozenia wynosi:%d\n", wynik);
 
 }
 void dzielenie()
 {
-    int a;
-    int b;
+    float a;
+    float b;
     float wynik;
-    printf_s("Podaj liczbę pierwsza oraz druga(wynik bedzie podany z dwoma liczbami po przecinku)\n");
-    scanf_s("%d, %d" & a, &b);
-    a / b = wynik;
+    printf_s("Podaj liczbę pierwsza oraz druga(Po przecinku)\n");
+    scanf_s("%f, %f", &a, &b);
+    wynik = a / b;
     printf_s("Wynik mnozenia wynosi:%.2f\n", wynik);
 
 }
 
 
 void menu() {
-   
+
     printf_s("\n");
     printf_s("Wybierz dzialanie jakie chcesz wykonac\n");
     printf_s("1-Dodawanie\n");
@@ -64,8 +74,10 @@ void menu() {
 
 int main(void)
 {
+    char opcja = 0;
     while (opcja != '5')
     {
+        
         menu();
         opcja = _getch();
         switch (opcja)
@@ -73,13 +85,26 @@ int main(void)
         case '1':
             dodawanie();
             break;
-             
-            
+        case '2':
+            odejmowanie();
+            break;
+        case '3':
+            mnozenie();
+            break;
+        case '4':
+            dzielenie();
+            break;
+        case '5':
+            exit(0);
+            break;
+        default: printf_s("\nWybierz dobra odp\n");
+
         }
 
 
+}
 
 
-
-    return 0;
+        return 0;
+    
 }
